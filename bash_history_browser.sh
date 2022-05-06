461ba93a7a5d6f9491d759a9bbb0056b02e84530
no382001 <s@x240>
2022-05-07 01:32:52 +0200

    fixed some issues with parsing

--- bash_history_browser.sh
+++ bash_history_browser.sh
@@ -1,100 +1,92 @@
 #!/bin/bash
 
-
-
 #main cycle
-programdone=0
-while [ $programdone -ne 1 ]
+HEIGHT=30
+WIDTH=25
+CHOICE_HEIGHT=4
+BACKTITLE="history browser"
+TITLE="1st layer"
+MENU="choose one of the following starting words:"
+
+OIFS=$IFS #save original delimiter
+IFS='\\\\'
+
+n=600 #number of elements
+
+#get the last n elements of history, invert them, and store them in his variable, delimited by \\\\
+#tail -$n
+all=$( cat ~/.bash_history | sort --unique |tac -s '\n' | awk '{print $0 "\\\\"}' )
+
+uniq=$( echo ${all[@]} )
+
+#get the first word
+firstlayer=$( echo $uniq | sed 's/|/ /' | awk '{print $1}' | sort --unique )
+
+#fomat them for the menu
+#dialog options format is (number1 "string1" number2 "string2" ...)
+fn=$( echo ${firstlayer[@]} | wc -l )
+i=1
+while [ $i -ne $fn ]
 do
+    #awk the current row
+    cur=($(echo ${firstlayer[@]}| awk -v i="$i" 'FNR == i {print $0;exit}'))
 
-    HEIGHT=30
-    WIDTH=15
-    CHOICE_HEIGHT=4
-    BACKTITLE="history browser"
-    TITLE="1st layer"
-    MENU="choose one of the following starting words:"
+    uniqopt=(${uniqopt[@]} $i $cur)
 
-    OIFS=$IFS #save original delimiter
-    IFS='\\\\'
-
-    n=100 #number of elements
-
-    #get the last n elements of history, invert them, and store them in his variable, delimited by \\\\
-    all=$( tail -$n ~/.bash_history | tac -s '\n' | awk '{print $0 "\\\\"}' )
-
-    uniq=$( echo ${all[@]} | sort --unique )
-
-    firstlayer=$( echo $uniq | sed 's/|/ /' | awk '{print $1}' | sort --unique )
-
-    #fomat them for the menu
-    #dialog options format is (number1 "string1" number2 "string2" ...)
-    fn=$( echo ${firstlayer[@]} | wc -l )
-    i=1
-    while [ $i -ne $fn ]
-    do
-        #awk the current row
-        cur=($(echo ${firstlayer[@]}| awk -v i="$i" 'FNR == i {print $0;exit}'))
-        
-        #add the row number
-        uniqopt=(${uniqopt[@]} $i)
-
-        #add the row content
-        uniqopt=(${uniqopt[@]} $cur)
-
-        #i++
-        i=$(($i+1)) 
-    done
-
-    #show first dialog and expect choice
-    CHOICE=$(dialog \
-                    --backtitle "$BACKTITLE" \
-                    --title "$TITLE" \
-                    --menu "$MENU" \
-                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                    ${uniqopt[@]} \
-                    2>&1 >/dev/tty)
-
-
-    CHOICE=${uniqopt[($CHOICE*2)-1]} #(choice*2)-1 transfer function to the dialog option format
-
-    #show second dialog choice
-    HEIGHT=30
-    WIDTH=130
-    CHOICE_HEIGHT=4
-    BACKTITLE="history browser"
-    TITLE="2nd layer"
-    MENU="choose one of the following commands:"
-
-    secondlayer=$( echo $uniq | grep "^$CHOICE" )
-
-    #fomat them for the menu
-    #dialog options format is (number1 "string1" number2 "string2" ...)
-
-    secuniqopt=()
-    sn=($(echo ${secondlayer[@]} | wc -l))
-
-    i=1
-    for k in ${secondlayer[@]}
-    do
-        #add the (number, row)
-        secuniqopt=(${secuniqopt[@]} $i $k)
-        
-        #i++
-        i=$(($i+1)) 
-    done
-
-
-
-    CHOICE=$(dialog \
-                    --backtitle "$BACKTITLE" \
-                    --title "$TITLE" \
-                    --menu "$MENU" \
-                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                    ${secuniqopt[@]} \
-                    2>&1 >/dev/tty)
-    clear
-
-    echo "-------------------"
-    echo ${secondlayer[$CHOICE-1]}
-    exit
+    #i++
+    i=$(($i+1)) 
 done
+
+
+#show first dialog and expect choice
+CHOICE=$(dialog \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                ${uniqopt[@]} \
+                2>&1 >/dev/tty)
+
+CHOICE=${uniqopt[($CHOICE*2)-1]} #(choice*2)-1 transfer function to the dialog option format
+
+#show second dialog choice
+HEIGHT=30
+WIDTH=130
+CHOICE_HEIGHT=4
+BACKTITLE="history browser"
+TITLE="2nd layer"
+MENU="choose one of the following commands:"
+
+secondlayer=$( echo $uniq | grep "^$CHOICE" )
+
+#fomat them for the menu
+#dialog options format is (number1 "string1" number2 "string2" ...)
+
+secuniqopt=()
+
+sn=($(echo ${secondlayer[@]} | wc -l))
+i=1
+while [ $i -ne $sn ]
+do
+    #awk the current row
+    cur=($(echo ${secondlayer[@]}| awk -v i="$i" 'FNR == i {print $0;exit}'))
+    
+    #add the (number, row)
+    secuniqopt=(${secuniqopt[@]} $i $cur)
+    #i++
+
+    i=$(($i+1)) 
+done
+
+echo ${secuniqopt[@]}
+
+CHOICE=$(dialog \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                ${secuniqopt[@]} \
+                2>&1 >/dev/tty)
+clear
+
+echo ${secuniqopt[($CHOICE*2)-1]}
